b344b30ab543bae309c84638809dd33de75ca4e2
beni <beniakbar555@gmail.com>
2024-12-05 23:25:40 +0700

    nyoba library tcpdf

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tikettransportasionline/tikettransportasionline',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '382144f0610a6d28f659f3e37d2d7875a99e5742',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.7.7',
+            'version' => '6.7.7.0',
+            'reference' => 'cfbc0028cc23f057f2baf9e73bdc238153c22086',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tikettransportasionline/tikettransportasionline' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '382144f0610a6d28f659f3e37d2d7875a99e5742',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
